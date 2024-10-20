82e390a3d3f3975749211d68061f2d546abb514d
nosmaster <nosmaster89@gmail.com>
2024-10-20 16:43:35 +0100

    fixed wifi ssid

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@
 int rgbpin = RGBpin;
 #endif
 const char *apSSID = "Erwin";
-const char *apPassword = "Erwin";
+const char *apPassword = "Erwin123";
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 0;
 const int daylightOffset_sec = 3600;
